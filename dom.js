1c1d6fd1991550fcb7e1500d2271bbbc190f9b73
Luka Popovic <lukapopovic0111@gmail.com>
2025-08-30 18:49:16 +0200

    Add main container, button styles and input styling

---
+++ dom.js
@@ -0,0 +1,10 @@
+export const domElements = {
+  btnEpisodes: document.querySelector('.episodes-btn'),
+  btnCharacters: document.querySelector('.characters-btn'),
+  btnLocations: document.querySelector('.locations-btn'),
+  searchEpisodes: document.querySelector('.search-epizodes'),
+  searchCharacters: document.querySelector('.search-characters'),
+  searchLocations: document.querySelector('.search-locations'),
+  list: document.querySelector('.list'),
+  main: document.querySelector('.container'),
+};
