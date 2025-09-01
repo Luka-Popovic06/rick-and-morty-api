b6abe087e4bdcae7a696faab2e307dd8db2bb5f9
Luka Popovic <lukapopovic0111@gmail.com>
2025-09-01 20:13:52 +0200

    Add fetchRickAndMortyData function for fetching data from API

--- api.js
+++ api.js
@@ -1,2 +1,20 @@
 import { domElements } from './dom.js';
 import { episode, character, location } from './input.js';
+export const fetchRickAndMortyData = async (number, type) => {
+  try {
+    for (let i = 0; i < number; i++) {
+      const rickAndMortyData = await fetch(
+        `https://rickandmortyapi.com/api/${type}/?page=${i}`
+      );
+      const list = await rickAndMortyData.json();
+      const { results } = list;
+      const names = results.map(n => n.name);
+      names.forEach(element => {
+        createListItem(element);
+      });
+      console.log(list);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
