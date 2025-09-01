e92d1668fc6dcb11024b0f0651f45bc25c26924f
Luka Popovic <lukapopovic0111@gmail.com>
2025-09-01 21:51:10 +0200

    Add getCharacter function to fetch and render character by name

--- api.js
+++ api.js
@@ -22,3 +22,20 @@ const createListItem = p => {
   const html = `<li class="list-item">${p}</li>`;
   domElements.list.insertAdjacentHTML('beforeend', html);
 };
+export const getCharacter = async characterName => {
+  try {
+    const characterData = await fetch(
+      `https://rickandmortyapi.com/api/character/?name=${characterName}`
+    );
+    const character = await characterData.json();
+    const { results } = character;
+    results.forEach(result => {
+      const { id, name, status, species, type, image } = result;
+      createCaracterInf(image, name, species, type, id, status);
+    });
+    console.log(character);
+    //
+  } catch (error) {
+    console.log(error);
+  }
+};
